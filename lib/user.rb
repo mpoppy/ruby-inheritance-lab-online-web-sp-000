e377f3db10ae5ac3e3f800480d30b0ca7f1467ab
Marc Poppy <poppy.marc@gmail.com>
2019-07-02 04:53:21 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,9 +3,9 @@ class User
 
   attr_accessor :firstname, :lastname
 
-  # def initalize(firstname, lastname)
-  #   @firstname = firstname
-  #   @lastname = lastname
-  # end
+  def initalize(firstname, lastname)
+    @firstname = firstname
+    @lastname = lastname
+  end
 
 end
